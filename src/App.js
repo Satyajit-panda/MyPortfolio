c618453bfc48ef103d0bb2a56de83a6af5edfbfe
satyajit <satyajitpanda293@gmail.com>
2021-06-26 21:14:12 +0530

    initial commit completed myportfolio component

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './App.css';
+//components
+import MyNavbar from"./components/my-navbar/my-navbar.component";
+
+const App =() =>
+{
+  return (
+    <div>
+        <MyNavbar/>
+        MY portfholio
+    </div>
+  );
+}
+
+export default App;
