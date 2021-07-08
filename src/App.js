560100ef9907ce1a9a7691fc1c67ed571d4f28eb
satyajit <satyajitpanda293@gmail.com>
2021-07-08 22:02:12 +0530

    Final upload

--- src/App.js
+++ src/App.js
@@ -13,6 +13,11 @@ import About from "./pages/about/about.component";
 import Skills from "./pages/skills/skills.component";
 import Experience from "./pages/experience/experience.component";
 import TimeLine from "./components/projects-timeline/projects-timeline.component";
+import ContactForm from "./pages/contact-form/contact-form.component";
+import FooterPanel from "./components/footer/footer.component";
+import Particles from "react-particles-js";
+import { particlesOptions } from "./particlesOption";
+
 
 
 const App =() =>
@@ -22,9 +27,13 @@ const App =() =>
         <MyNavbar/>
         <MyCarousal/>
         <TitleMessage/>
+        <Particles
+          className="particles particles-box"
+          params={particlesOptions}
+        />
         {/* About Me Section */}
         <div>
-<Parallax blur = {{ min: -1000, max: 1000}}
+        <Parallax blur = {{ min: -1000, max: 1000}}
           bgImage={BgImage}
           bgImageAlt="parallax bg"
           strength={-200}>
@@ -32,6 +41,7 @@ const App =() =>
           <Fade duration={500}>
           <About/>
           </Fade>
+          <hr/>
           </Container>
           </Parallax>
           </div>
@@ -39,22 +49,41 @@ const App =() =>
           <div>
           <Container className="container-box rounded">
           <Fade duration={500}>
+          <hr/>
           <Skills/>
           </Fade>
           </Container>
           </div>
+           {/*Experience*/}
           <div>
           <Container className="container-box rounded">
           <Fade duration={500}>
+          <hr/>
           <Experience/>
           </Fade>
           </Container>
           </div>
+          {/*TimeLine*/}
+          <div>
           <Container className="container-box rounded">
           <Fade duration={500}>
+          <hr/>
           <TimeLine/>
           </Fade>
           </Container>
+          </div>
+          {/*TimeLine*/}
+          <div>
+          <Container className="container-box rounded">
+          <Fade duration={500}>
+          <hr/>
+          <ContactForm/>
+          </Fade>
+          </Container>
+          </div>
+          <hr/>
+          <FooterPanel/>
+
           </div>
 
   );
